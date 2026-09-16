09b75adeeca8a768befd10f119640151a02178e8
Ariiq354 <realgoldcraft@gmail.com>
2026-09-16 16:56:15 +0700

    first commit

---
+++ src/app/(abcd)/subscriptions/[id].tsx
@@ -0,0 +1,15 @@
+import { Link, useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+const SubscriptionDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+
+  return (
+    <View>
+      <Text>Subscriptions Detail: {id}</Text>
+      <Link href="/">Go Back</Link>
+    </View>
+  );
+};
+
+export default SubscriptionDetails;
